2ddb84c800b54cc466ab1da28a822327659214b4
DanielMor <danielmoreira92@gmail.com>
2015-02-18 15:55:05 +0000

    update
    changes

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ var express = require('express')
     , server = http.createServer(app)
     , io = require('socket.io').listen(server);
 
-server.listen(13100);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+server.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+});
 
 app.use(express.static(__dirname + '/'));
 
